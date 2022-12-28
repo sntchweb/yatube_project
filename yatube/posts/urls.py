ee2055a533f1c29e3dfc6adff59dffc7e9a86735
Артём Лашин <sntchweb@yandex.ru>
2022-12-28 10:45:25 +0400

    added gropus, slug, update views

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,6 +6,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/', views.group_posts, name='group'),
-    path('group/<slug:pk>/', views.post_detail)
+    path('group/<slug:slug>', views.group_posts, name='group_list'),
+    # path('group/<slug:pk>/', views.post_detail)
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': "Это главная страница проекта Yatube"
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
-def group_posts(request):
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': "Здесь будет информация о группах проекта Yatube"
+        'group': group,
+        'posts': posts
     }
     return render(request, 'posts/group_list.html', context)
 
-def post_detail(request, pk):
-    return HttpResponse(f'Страница c новостью {pk}')
+# def post_detail(request, pk):
+#     return HttpResponse(f'Страница c новостью {pk}')
